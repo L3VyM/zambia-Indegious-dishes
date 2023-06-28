87d9986838af86f4e67e8131235343f4cf443fa2
Levy Mbewe <59606483+L3VyM@users.noreply.github.com>
2023-06-28 19:09:34 +0000

    ui content updates

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,30 +1,31 @@
 function Content(){
     return (
-        <div class="container d-flex justify-content-center  mt-5 col-12 col-lg-12">
-            <div class="row">
-              <div class="col-md-6 stretch-card grid-margin mt-2" >
-                <div class="card">
-                <img src={process.env.PUBLIC_URL + '/images/pumpkin_leaves.jpg'} alt="Pumpkin Leaves" />;
-                  <div class="card-body">
-                    <h3 class="card-title">Chibwabwa (Pumpkin Leaves)</h3>
-                    {/* <p class="card-text">Description.</p> */}
-                    <a href={"#"} class="btn btn-primary">Learn more</a>
-                  </div>
-                </div>
-              </div>
-              
-              <div class="col-md-6 stretch-card grid-margin mt-2">
-                <div class="card">
-                <img src={process.env.PUBLIC_URL + '/images/blackjack.jpg'} alt="Pumpkin Leaves" />;
-                  <div class="card-body">
-                    <h3 class="card-title">Kanunka (Blackjack)</h3>
-                    {/* <p class="card-text">Description.</p> */}
-                    <a href={"#"} class="btn btn-primary">Learn more</a>
-                  </div>
-                </div>
-              </div>
+      <div className="container d-flex justify-content-center mt-3 col-12 col-lg-12">
+      <div className="row">
+        <div className="col-md-6 stretch-card grid-margin mt-2 mb-4 shadow-sm p-3 mb-2 bg-body rounded">
+          <div className="card">
+            <img src={process.env.PUBLIC_URL + '/images/pumpkin_leaves.jpg'} alt="Pumpkin Leaves" />
+            <div className="card-body">
+              <h3 className="card-title">Chibwabwa (Pumpkin Leaves)</h3>
+              {/* <p className="card-text">Description.</p> */}
+              <a href="#" className="btn btn-primary">Learn more</a>
             </div>
+          </div>
         </div>
+    
+        <div className="col-md-6 stretch-card grid-margin mt-2 mb-4 shadow-sm p-3 mb-2 bg-body rounded">
+          <div className="card">
+            <img src={process.env.PUBLIC_URL + '/images/blackjack.jpg'} alt="Pumpkin Leaves" />
+            <div className="card-body">
+              <h3 className="card-title">Kanunka (Blackjack)</h3>
+              {/* <p className="card-text">Description.</p> */}
+              <a href="#" className="btn btn-primary">Learn more</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
     )
 }
 
